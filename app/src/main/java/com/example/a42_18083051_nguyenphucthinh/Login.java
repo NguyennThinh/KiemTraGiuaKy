90621e598fd3c785d3373e886537802cc84e10ef
Nguyen Thinh <nguyenphucthinh1422001@gmail.com>
2021-11-05 13:58:09 +0700

    Hoan thanh cau 3

--- app/src/main/java/com/example/a42_18083051_nguyenphucthinh/Login.java
+++ app/src/main/java/com/example/a42_18083051_nguyenphucthinh/Login.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -47,14 +48,32 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+                String email = edtEmail.getText().toString();
+                String pass = edtPass.getText().toString();
+                if (email.equals("nguyenthinhc9@gmail.com") && pass.equals("123456789")){
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(Login.this, "Sai email hoac password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
